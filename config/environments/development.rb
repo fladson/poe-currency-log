e7192d13dcdba54d5806ec03d7479e8bbe6c8924
Fladson Gomes <fladsonthiago@gmail.com>
2018-02-22 00:10:53 -0300

    Setup email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,6 +52,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener
+
   config.after_initialize do
     Bullet.enable = true
     Bullet.honeybadger = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "poe-currency-log_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = {
+    host: ENV["HEROKU_DOMAIN"]
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: ENV["HEROKU_DOMAIN"],
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
