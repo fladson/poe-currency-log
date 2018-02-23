3435d88674faf3394028bc198229178c7ea1ec52
Fladson Gomes <fladsonthiago@gmail.com>
2018-02-23 16:09:38 -0300

    Add monitoring gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_raven_context
 
   protected
 
@@ -13,5 +14,12 @@ class ApplicationController < ActionController::Base
       :account_update,
       keys: [:session, :account_name]
     )
+  end
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: current_user.id, email: current_user.email) unless current_user.blank?
+    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
 end
