b6f26e953df54f89e1041bfdabd6e5e7266dd4f3
Fladson Gomes <fladsonthiago@gmail.com>
2018-02-22 23:09:21 -0300

    Redirect to dashboard after signin, signup and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
-  get "pages/home"
-  get "pages/dashboard"
 
   devise_for :users
+  as :user do
+    get "dashboard", to: "pages#dashboard", as: :user_root
+  end
 end
