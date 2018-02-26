e87665d3de8bee4601ca712aac1dc559202f287c
Fladson Gomes <fladsonthiago@gmail.com>
2018-02-26 16:11:03 -0300

    Remove account_name as input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
-      :sign_up,
-      keys: [:session, :account_name]
+      :sign_up, keys: [:session]
     )
     devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: [:session, :account_name]
+      :account_update, keys: [:session]
     )
   end
 
