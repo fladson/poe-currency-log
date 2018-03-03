77b7172728034180afe1d43bc91a77bc89bdca39
Fladson Gomes <fladsonthiago@gmail.com>
2018-03-03 09:47:48 -0300

    Add data uniqueness to CurrencyLog

--- app/models/currency_log.rb
+++ app/models/currency_log.rb
@@ -1,3 +1,4 @@
 class CurrencyLog < ApplicationRecord
   belongs_to :user
+  validates_uniqueness_of :data, scope: [:user_id, :league]
 end
--- spec/models/currency_log_spec.rb
+++ spec/models/currency_log_spec.rb
@@ -2,4 +2,5 @@ require "rails_helper"
 
 RSpec.describe CurrencyLog, type: :model do
   it { is_expected.to belong_to(:user) }
+  it { is_expected.to validate_uniqueness_of(:data).scoped_to([:user_id, :league]) }
 end
