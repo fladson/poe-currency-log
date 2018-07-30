4c040d7482fdbb89d4ad10669dfd490f90f243ce
Fladson Gomes <fladsonthiago@gmail.com>
2018-07-30 15:42:15 -0300

    Add league route to PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,8 +7,16 @@ class PagesController < ApplicationController
   def dashboard
   end
 
+  def league
+    render partial: "poe/currency", locals: { league:params[:league] }
+  end
+
   def fetch_currency
     LogCurrencyWorker.perform_async(current_user.email)
     redirect_back(fallback_location: user_root_path)
+  end
+
+  def page_params
+    params.permit(:league)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "fetch_currency", to: "pages#fetch_currency", as: :fetch_currency
+  get "dashboard/:league", to: "pages#league", as: :league
 
   as :user do
     get "dashboard", to: "pages#dashboard", as: :user_root
