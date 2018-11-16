91281b15b4bca61f930fc57ff225482c6c09234f
Fladson Gomes <fladsonthiago@gmail.com>
2018-11-16 15:44:05 -0300

    Add terms and privacy pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,6 +14,18 @@ class PagesController < ApplicationController
     redirect_back(fallback_location: user_root_path)
   end
 
+  def leagues_comparison
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def help
+  end
+
   def page_params
     params.permit(:league)
   end
