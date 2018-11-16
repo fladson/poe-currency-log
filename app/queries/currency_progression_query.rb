7b68e4cdbc79eaea972dad2cd27e356068ebea9b
Fladson Gomes <fladsonthiago@gmail.com>
2018-11-16 15:56:46 -0300

    Add without date methods to CurrencyProgressionQuery

--- app/queries/currency_progression_query.rb
+++ app/queries/currency_progression_query.rb
@@ -9,6 +9,10 @@ class CurrencyProgressionQuery
     @relation.timeline_order.by_league(league).formatted
   end
 
+  def without_date(league)
+    @relation.timeline_order.by_league(league).formatted_without_date
+  end
+
   module Chart
     def self.inject_created_at((data, created_at))
       data.each_with_object({}) do |(key, value), hash|
@@ -22,10 +26,21 @@ class CurrencyProgressionQuery
       end
     end
 
+    def self.new_accumulate(acc, data)
+      acc.each_with_object({}) do |(key, value), hash|
+        hash[key] = [*data[key]].push(value).flatten
+      end
+    end
+
     def self.generate(relation, field, date = :created_at)
       relation.pluck(field, date)
         .map(&method(:inject_created_at))
         .inject(&method(:accumulate))
+    end
+
+    def self.generate_without_date(relation, field)
+      relation.pluck(field)
+        .inject(&method(:new_accumulate))
     end
   end
 
@@ -40,6 +55,10 @@ class CurrencyProgressionQuery
 
     def formatted
       Chart.generate(self, :data)
+    end
+
+    def formatted_without_date
+      Chart.generate_without_date(self, :data)
     end
   end
 end
