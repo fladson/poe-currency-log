c2dd5d93b63f674f9b6d9605d1fec6490f911109
Fladson <fladsonthiago@gmail.com>
2019-03-21 22:16:43 -0300

    Add ApplicationHelper spec

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def flash_class(level)
     case level
-    when 'notice' then "bg-blue-lightest border-blue"
-    when 'success' then "bg-green-lightest border-green"
-    when 'error' then "bg-red-lightest border-red"
-    when 'alert' then "bg-orange-lightest border-orange"
+    when 'notice'  then 'bg-blue-lightest border-blue'
+    when 'success' then 'bg-green-lightest border-green'
+    when 'error'   then 'bg-red-lightest border-red'
+    when 'alert'   then 'bg-orange-lightest border-orange'
     end
   end
 end
---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  describe 'flash_class' do
+    subject { flash_class(level) }
+
+    context 'when level is notice' do
+      let(:level) { 'notice' }
+
+      it 'returns the blue class' do
+        expect(subject).to eq('bg-blue-lightest border-blue')
+      end
+    end
+
+    context 'when level is success' do
+      let(:level) { 'success' }
+
+      it 'returns the blue class' do
+        expect(subject).to eq('bg-green-lightest border-green')
+      end
+    end
+
+    context 'when level is error' do
+      let(:level) { 'error' }
+
+      it 'returns the blue class' do
+        expect(subject).to eq('bg-red-lightest border-red')
+      end
+    end
+
+    context 'when level is alert' do
+      let(:level) { 'alert' }
+
+      it 'returns the blue class' do
+        expect(subject).to eq('bg-orange-lightest border-orange')
+      end
+    end
+  end
+end
