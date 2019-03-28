3e7b840bbdb060d585763c132adfc7f1b53e6d4f
Fladson <fladsonthiago@gmail.com>
2019-03-28 14:54:49 -0300

    Prevent sortable undefined on other pages

--- app/assets/javascripts/sortable.js
+++ app/assets/javascripts/sortable.js
@@ -1,14 +1,15 @@
 document.addEventListener("turbolinks:load", function(){
   var el = document.getElementById('sortable');
-  var sortable = Sortable.create(el, {
-    handle: '.feather-move',
-    animation: 150,
+  if(el) {
+    var sortable = Sortable.create(el, {
+      handle: '.feather-move',
+      animation: 150,
 
-    onUpdate: function (evt) {
-      [].forEach.call(evt.from.getElementsByClassName('sort'), function (el,index) {
-        console.log(el, index)
-        el.setAttribute("value",index);
-      });
-    },
-  });
+      onUpdate: function (evt) {
+        [].forEach.call(evt.from.getElementsByClassName('sort'), function (el, index) {
+          el.setAttribute("value", index);
+        });
+      },
+    });
+  }
 });
